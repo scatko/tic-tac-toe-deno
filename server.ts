2483340210e5b7454bd352ada1d696255fb5461e
kazuriki <kazuriki@yandex.ru>
2020-12-03 16:33:04 +0300

    basic http server from tutorial

--- server.ts
+++ server.ts
@@ -1,1 +1,12 @@
+import { serve } from "https://deno.land/std/http/server.ts";
+
+const server = serve({ hostname: "0.0.0.0", port: 8080 });
 console.log("privetik");
+console.log(`HTTP webserver running.  Access it at:  http://localhost:8080/`);
+
+for await (const request of server) {
+  let bodyContent = "Your user-agent is:\n\n";
+  bodyContent += request.headers.get("user-agent") || "Unknown";
+
+  request.respond({ status: 200, body: bodyContent });
+}
