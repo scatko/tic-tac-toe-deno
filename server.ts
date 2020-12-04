63b7102c152172a5b0df1bad072b89132dc567ab
kazuriki <kazuriki@yandex.ru>
2020-12-04 13:41:20 +0300

    add port to args

--- server.ts
+++ server.ts
@@ -1,8 +1,10 @@
 import { serve } from "https://deno.land/std/http/server.ts";
 
-const server = serve({ hostname: "0.0.0.0", port: 8080 });
-console.log("privetik");
-console.log(`HTTP webserver running.  Access it at:  http://localhost:8080/`);
+const DEFAULT_HTTP_PORT = 8080;
+
+const port = Number(Deno.args[0]) || DEFAULT_HTTP_PORT;
+const server = serve({ hostname: "0.0.0.0", port });
+console.log(`HTTP webserver running. Access it at: http://localhost:${port}/`);
 
 for await (const request of server) {
   let bodyContent = "Your user-agent is:\n\n";
